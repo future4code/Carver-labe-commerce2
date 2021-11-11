4f196a3e6fc9ded008a8b38656a9ab43ac1df206
Stefani <stefani.barros28@gmail.com>
2021-11-11 14:19:43 -0300

    falta implementar o carrinho

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components'
-import Produto from './components/Produto/Produto';
+import { produtos } from './components/Produto/Produto';
+import Carrinho from './components/Carrinho/Carrinho';
+import CardProduto from './components/Produto/CardProduto';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
@@ -13,22 +15,71 @@ const GlobalStyle = createGlobalStyle `
 `
 
 const MainContainer = styled.div`
-  display: flex;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 6fr 1fr;
+
 `
 
-function App () {
+const AreaProdutos = styled.div `
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat()(4, 1fr);
+`
+
+export default class App extends React.Component{
+  state = {
+    carrinho: [],
+    produtos: produtos,
+    listaProdutos: true
+  }
+
+  acrescentarProdutoCarrinho = (produto) => {
+    const carrinhoAtual = [...this.state.carrinho]
+    const itemNoCarrinho = carrinhoAtual.findIndex(item => item.id === produto.id);
+
+    if (itemNoCarrinho) {
+      const produtoAdicionado = this.state.carrinho.map((item) => {
+        return {...item,
+          quantidade: carrinhoAtual[itemNoCarrinho].quantidade + 1
+        }
+      })
+      this.setState({ carrinho:produtoAdicionado })
+    } else {
+        carrinhoAtual.push({ ...produto, quantidade:1 })
+        this.setState({ carrinho: carrinhoAtual })
+    }
+  }
+
+  paginaCarrinho = () => {
+    this.setState({ listaProdutos: !this.state.listaProdutos })
+  }
+
+  render() {
+    const teste = this.state.produtos.map((produto) => {
+          return(
+              <CardProduto
+                 key={produto.id}  
+                 image={produto.image}              
+                 nome={produto.nome}   
+                 valor={produto.valor}
+                 acrescentarProdutoCarrinho ={() => this.acrescentarProdutoCarrinho(produto.id)}
+              />
+            
+          )
+    });
+
     return (
       <div>
         <GlobalStyle />
         <Header/>
         <MainContainer>
-          <Produto />
+          <AreaProdutos>
+            { teste }
+          </AreaProdutos> 
+          <Carrinho />
         </MainContainer>
         <Footer/>
       </div>
     );
-  
+  }
 }
-
-export default App;
--- src/components/Carrinho/Carrinho.js
+++ src/components/Carrinho/Carrinho.js
@@ -1,11 +1,53 @@
 import React from 'react';
 
-export class Carrinho extends React.Component {
+export default class Carrinho extends React.Component {
+    valorTotalProduto = (preco, quantidade) => {
+        const total = (preco * quantidade);
+        return total;
+    }
+
+    valorTotalCarrinho = (listaProdutos) => {
+        let valorTotal = 0;
+        for (let produto of listaProdutos) {
+            valorTotal += produto.preco * produto.quantidade
+        }
+        return valorTotal;
+    }
+ 
     render() {
+
+        // const produtosNoCarrinho = this.props.carrinho.map((produto) => {
+        //     return (
+        //         <div key={produto.id}>
+        //             <img src={produto.image} alt="Nave"/>
+        //         </div>
+        //     )
+        // })
+
+        // const areaCarrinho  = this.props.carrinho.length;
+
+        // if (areaCarrinho > 0) {
+        //     return (
+        //         <div>
+        //             <div>
+        //                 <h1>Itens Adicionados</h1>
+        //             </div>
+        //             {produtosNoCarrinho}
+        //         </div>
+        //     );    
+        // }else {
+        //     return (
+        //         <div>
+        //             <h2>Carrinho Vazio</h2>
+        //         </div>
+        //     )   
+        // }
+
         return (
             <div>
-                
+                <h2>Carrinho Vazio</h2>
             </div>
         )
+
     }
 }
