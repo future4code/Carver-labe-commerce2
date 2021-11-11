d8bd01686291f438106c50ccfe8e0c824810df31
Stefani <stefani.barros28@gmail.com>
2021-11-11 20:04:36 -0300

    falta fazer o botao funcionar

--- src/components/Carrinho/ListagemCarrinho.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Carrinho from './Carrinho'
-
-
-export default class ListagemCarrinho extends React.Component {
-    render() {
-        return()
-    }
-}
--- src/components/Filtro/Filtro.js
+++ src/components/Filtro/Filtro.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import { produtos } from "../Produto/Produto";
 
 export class Filtro extends React.Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  state = {
+    produtos: produtos,
+    query: "",
+    precoMinimo: "",
+    precoMaximo: "",
+    sortingParameter: "nome",
+    order: 1,
+  };
+
+  onchangeQuery = (event) => {
+    this.setState({
+      query: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+        <div>
+
+        </div>
+    )
+  }
 }
