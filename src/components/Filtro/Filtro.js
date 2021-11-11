237271e513d32bd10393d8409be1baebd88f73de
Ruanar <ruana.piber@gmail.com>
2021-11-11 18:12:35 -0300

    iniciando a implementação dos filtros

--- src/components/Filtro/Filtro.js
+++ src/components/Filtro/Filtro.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import Produto from '../Produto/Produto';
 
 export class Filtro extends React.Component {
+    state = {
+        produtos: produtos,
+        query: "",
+        precoMinimo: "",
+        precoMaximo: "",
+        sortingParameter: "nome",
+        order: 1
+    }
+    
+    onchangeQuery = (event) => {
+        this.setState({
+            query: event.target.value
+        })
+    }
+    
     render() {
         return (
             <div>
