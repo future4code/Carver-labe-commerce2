164025960683cf6ec1788284333d7dc4a70d5b37
Stefani <stefani.barros28@gmail.com>
2021-11-13 20:45:24 -0300

    modificando algumas partes do código

--- src/components/Filtro/Filtro.js
+++ src/components/Filtro/Filtro.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { produtos } from '../Produto/Produto';
-import CardProduto from '../Produto/CardProduto';
 
 export const BarraFiltros = styled.div`
     display: flex;
@@ -14,38 +12,23 @@ export const BarraFiltros = styled.div`
 `
 
 
-const ListaDeProdutos = produtos.map((produto) => {
-        return(
-            <CardProduto
-               key={produto.id}  
-               image={produto.image}              
-               nome={produto.nome}   
-               valor={produto.valor}
-               adicionarProduto = {() => this.adicionarProduto(produto.id)}
-            />
-        )}
-)
-
-export function Filtros(props) {
-    return <div>
-        <input
-            placeholder="Busca por nome"
-            value={props.query}
-            onChange={props.onchangeQuery}
-        />
-
+export default class Filtro extends React.Component {
+    
+     render() {
+        return (
+            <div>
         <input
             type="number"
             placeholder="Preço mínimo"
-            value={props.precoMinimo}
-            onChange={props.onchangePrecoMinimo}
+            value={this.props.precoMinimo}
+            onChange={this.props.onchangePrecoMinimo}
             />
 
         <input
             type="number"
             placeholder="Preço máximo"
-            value={props.precoMaximo}
-            onChange={props.onchangePrecoMaximo}
+            value={this.props.precoMaximo}
+            onChange={this.props.onchangePrecoMaximo}
             />
 
         <span>
@@ -53,94 +36,13 @@ export function Filtros(props) {
             <select
             
             name="order"
-            value={props.order}
-            onChange={props.onchangeOrder}
+            value={this.props.order}
+            onChange={this.props.onchangeOrder}
             >
                 <option value={1}>Preço - Crescente</option>
                 <option value={-1}>Preço - Decrescente</option> 
             </select>
         </span>
-    
-    </div>
-    
-}
-
-
-export class Filtro extends React.Component {
-    state = {
-        produtos: ListaDeProdutos,
-        query: "",
-        precoMinimo: "",
-        precoMaximo: "",
-        sortingParameter: "crescente",
-        order: 1
-    }
-    
-    onchangeQuery = (event) => {
-        this.setState({
-            query: event.target.value
-        })
-    }
-
-    onchangePrecoMinimo = (event) => {
-        this.setState({
-            precoMinimo: event.target.value
-        })
-    }
-    onchangePrecoMaximo = (event) => {
-        this.setState({
-            precoMaximo : event.target.value
-        })
-    }
-    onchangeSortingParameter = (event) => {
-        this.setState({
-            order: event.target.value
-        })
-    }
-    onchangeOrder = (event) => {
-        this.setState({
-            order: event.target.value
-        })
-    }
-    
-    render() {
-        return (
-            <div>
-                <Filtros>
-                query={this.state.query}
-                onchangeQuery={this.onchangeQuery}
-                onchangePrecoMinimo={this.onchangePrecoMinimo}
-                onchangePrecoMaximo={this.onchangePrecoMaximo}
-                onchangeSortingParameter={this.onchangeSortingParameter}
-                onchangeOrder={this.onchangeOrder}
-                precoMinimo={this.state.precoMinimo}
-                precoMaximo={this.state.precoMaximo}
-                sortingParameter={this.state.sortingParameter}
-                order={this.state.order}
-                </Filtros>
-                <div>
-                    {this.state.produto
-                    .filter(produto => {
-                        return produto.nome.toLowerCase().includes(this.state.query.toLowerCase())
-                    })
-                    .filter(produto => {
-                        return this.state.precoMinimo === "" || produto.valor >= this.state.precoMinimo
-                    })
-                    .filter(produto => {
-                        return this.state.precoMaximo === "" || produto.valor <= this.state.precoMaximo
-                    })
-                    .sort((a,b) => {
-                        switch (this.state.sortingParameter) {
-                            case "crescente":
-                                return this.state.order * (a.valor - b.valor)
-                            case "decrescente":
-                                return this.state.order *(b.valor - a.valor)    
-                                break;
-                        }
-                    })
-                   
-                    }
-                </div>
             </div>
         )
     }
